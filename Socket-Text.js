559dd7611c6ada09221a94db48196414888449b7
Kyungrok <kllee@huconn.com>
2023-08-10 16:05:54 +0900

    [module] - css추가

--- Socket-Text.js
+++ Socket-Text.js
@@ -4,16 +4,21 @@ Module.register("Socket-Text", {
         imageSrc: "image/music.png" // 이미지 파일을 모듈 디렉토리에 위치시킴
     },
 
+    getStyles: function() {
+        return ["styles.css"]; // 연결할 CSS 파일 이름
+    },
+
     getDom: function() {
         var wrapper = document.createElement("div");
-
         var image = document.createElement("img");
+        var textWrapper = document.createElement("div");
+
         image.src = this.data.path + "/" + this.config.imageSrc; // 상대 경로 설정
         wrapper.appendChild(image);
-    
-        // 텍스트를 포함하는 div 영역 생성
-        var textWrapper = document.createElement("div");
+        image.className = 'image-wrapper'
+
         textWrapper.innerHTML = this.config.text;
+        textWrapper.className = 'text';
         wrapper.appendChild(textWrapper);
     
         return wrapper;
