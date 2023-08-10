e98a4aaa5ffc895f0a44b0cf2e848f08a4be05b7
Kyungrok <kllee@huconn.com>
2023-08-10 14:31:56 +0900

    [module] - 이미지 추가

--- Socket-Text.js
+++ Socket-Text.js
@@ -1,10 +1,15 @@
 Module.register("Socket-Text", {
     defaults: {
         text: "Hello World!",
+        imageSrc: "image/music.png" // 이미지 파일을 모듈 디렉토리에 위치시킴
     },
 
     getDom: function() {
         var wrapper = document.createElement("div");
+
+        var image = document.createElement("img");
+        image.src = this.data.path + "/" + this.config.imageSrc; // 상대 경로 설정
+        wrapper.appendChild(image);
     
         // 텍스트를 포함하는 div 영역 생성
         var textWrapper = document.createElement("div");
